2e0bc28eddf43bdb77caa2d3e14a278e338a7575
Israfil <6347israfil@gmail.com>
2022-09-24 18:51:58 +0600

    api collection

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Reviews;
 
 class ProductResource extends JsonResource
 {
@@ -18,8 +19,13 @@ class ProductResource extends JsonResource
             'name'=> $this->name,
             'desctiption'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock == 0 ?'out of stock': $this->stock,
             'discount'=>$this->discount,
+            'totalprice'=>round((1-($this->discount/100))*$this->price,2),
+            'rating'=>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Reating yet',
+            // 'href'=>[
+            //      'reviews'=> route('reviews.index',$this->id)
+            // ]
         ];
         // parent::toArray($request);
     }
